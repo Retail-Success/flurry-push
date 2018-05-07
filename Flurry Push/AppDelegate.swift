b07b49d9b9d443c9b25ec7e693f742e4030fad58
Don Huff <donh@posim.com>
2018-05-07 14:19:55 -0500

    Log messages and add readme explaining issues.

--- Flurry Push/AppDelegate.swift
+++ Flurry Push/AppDelegate.swift
@@ -7,40 +7,87 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
     var window: UIWindow?
-
-
+    
+    // MARK: - UIApplicationDelegate
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let builder = FlurrySessionBuilder()
+            .withCrashReporting(true)
+            .withLogLevel(FlurryLogLevelCriticalOnly)
+        let apiKey = API_KEY_HERE
+        Flurry.startSession(apiKey, with: builder)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .sound, .badge]
+        notificationCenter.requestAuthorization(options: authOptions) { (granted, error) in
+            guard granted,
+                error == nil else {
+                    assertionFailure()
+                    let message: String
+                    if let e = error {
+                        message = String(describing: e)
+                    } else {
+                        message = "unknown"
+                    }
+                    NSLog("UNUserNotificationCenter authorization failed: \(message).")
+                    return
+            }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        NSLog("application(_:didReceiveRemoteNotification:fetchCompletionHandler:)")
+        // our app saves notification data here
+        completionHandler(.noData)
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let hex = deviceToken.reduce(into: "") { $0.append(String(format:"%02x", $1)) }
+        NSLog("Remote notification device token: \(hex)")
+        FlurryMessaging.setDeviceToken(deviceToken)
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    // MARK: - UNUserNotificationCenterDelegate
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        guard FlurryMessaging.isFlurryMsg(response.notification.request.content.userInfo) else {
+            assertionFailure()
+            return
+        }
+        
+        NSLog("Calling FlurryMessaging.receivedNotificationResponse...")
+        FlurryMessaging.receivedNotificationResponse(response) {
+            NSLog("FlurryMessaging.receivedNotificationResponse complete.")
+            completionHandler()
+        }
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        guard FlurryMessaging.isFlurryMsg(notification.request.content.userInfo) else {
+            assertionFailure()
+            // present as an alert
+            completionHandler(.alert)
+            return
+        }
+        
+        NSLog("Calling FlurryMessaging.present...")
+        FlurryMessaging.present(notification) {
+            NSLog("FlurryMessaging.present complete.")
+            completionHandler(.alert)
+        }
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
-
---
+++ Flurry Push/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Flurry Push
+//
+//  Created by Don Huff on 5/7/18.
+//  Copyright © 2018 Retail Success. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <Flurry/Flurry.h>
+#import <FlurryMessaging/FlurryMessaging.h>
+
+#endif /* Bridging_Header_h */
